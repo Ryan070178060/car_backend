8d8ab90b1ab0c864a5d84626c1dad91b4410968e
Ryan Shamala <ryanshamala254@gmail.com>
2024-04-04 20:26:22 +0300

    ttt

--- index.js
+++ index.js
@@ -21,17 +21,9 @@ mongoose.connect("mongodb+srv://Ryan:shamala254@cluster0.brlg6co.mongodb.net", {
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
+// Serve static files from the 'frontend' directory
+app.use(express.static(path.join(__dirname, '/car-ecommerce')));
 
-// Serve static files from the 'car-ecommerce' directory
-const frontendDirectory = path.join(__dirname, 'car-ecommerce');
-console.log("Frontend directory:", frontendDirectory); // Debugging
-app.use(express.static(frontendDirectory));
-
-// Debugging middleware to log incoming requests
-app.use((req, res, next) => {
-  console.log("Incoming request:", req.url);
-  next();
-});
 
 
 // Image Storage Engine

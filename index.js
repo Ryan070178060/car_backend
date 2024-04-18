e5c9e48ce43dfebe6342ed1475437b4595acb46d
Ryan Shamala <ryanshamala254@gmail.com>
2024-04-18 16:05:13 +0300

    hh

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use('/images', express.static(path.join(__dirname, '/upload/images')));
 app.post("/upload", upload.single('product'), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:4000/images/${req.file.filename}`
+    image_url: `https://car-backend-1.onrender.com/images/${req.file.filename}`
   });
 });
 
